7bb222c6b58799365a4e6ae4c735e2bd7dbc6d92
char <965059585@qq.com>
2019-01-22 10:43:23 +0000

    Basic backend server
    Build the basic backend server

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+# game_backend
+Backend of the AI_Game
+
+Technical:
+- Spring Boot
+- IBM Personality Insight
+- MySQL
+
+API reference:
+- TBD
+
+TODO
+- Transfer the result of analysis to character attributes:
+	- user choose the personality
+	- compare the similarity
+	- generate the attributes
+	
+- Improve the security of database connection
---
+++ backend/src/main/java/springboot/service/PersonalityInsightService.java
@@ -0,0 +1,52 @@
+package springboot.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ibm.watson.developer_cloud.personality_insights.v3.PersonalityInsights;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.Content;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.ContentItem;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.Profile;
+import com.ibm.watson.developer_cloud.personality_insights.v3.model.ProfileOptions;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+
+import springboot.util.AnalysisConfig;
+import springboot.util.ContentLoader;
+
+@Service("piService")
+public class PersonalityInsightService {
+	private AnalysisConfig config = new AnalysisConfig();
+	private PersonalityInsights personalityInsights;
+	private Content content;
+	
+	public PersonalityInsightService() {
+		/*
+		 * authentication
+		 * token-based Identity and Access Management(IAM) 
+		 */
+		IamOptions options = new IamOptions.Builder().apiKey(config.getApiKey()).build();
+
+		personalityInsights = new PersonalityInsights(config.getVersion(), options);
+
+		personalityInsights.setEndPoint(config.getUrl());
+	}
+	
+	public Profile analysis() {
+		this.loadInput();
+		
+		ProfileOptions profileOptions = new ProfileOptions.Builder().content(content).consumptionPreferences(true).rawScores(true).build();
+		
+		Profile profile = personalityInsights.profile(profileOptions).execute();
+		
+		// result profile in JSON
+		return profile;
+	}
+	
+	public void loadInput() {
+		// Load input content
+		ContentLoader contentLoader = new ContentLoader();
+		
+		ContentItem contentItem = new ContentItem.Builder().content(contentLoader.getInput()).build();
+		
+		content = new Content.Builder().addContentItem(contentItem).build();
+	}
+}
