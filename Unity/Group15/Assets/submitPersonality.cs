b478a331a211e5db953e9ba9cdd839a84ff2222c
Unknown <psyam19@nottingham.ac.uk>
2019-01-24 16:26:36 +0000

    Added first scenes
    Added the first iteration of the entire Unity project - including Start/Settings and Character Setup screens.

---
+++ Unity/Group15/Assets/submitPersonality.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class submitPersonality : MonoBehaviour {
+
+    public Text totalLabel;
+
+    public void checkSubmission () {
+        float trait_total = float.Parse(totalLabel.text);
+        if (trait_total != 15)
+            Debug.Log("ERROR with totals.");
+        else
+            SceneManager.LoadScene("TwitterLogin");
+    }
+}
