23ba5abdbbce91621b01dacdd23658caa4058063
Unknown <psyam19@nottingham.ac.uk>
2019-01-25 08:38:38 +0000

    Added username/password entering w/o functionality

---
+++ Unity/Group15/Assets/Scripts/agreeTwitter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class agreeTwitter : MonoBehaviour {
+
+    public Toggle toggle;
+    public Button button;
+
+    void Update() {
+        if (toggle.isOn == true) {
+            button.enabled = true;
+            button.GetComponentInChildren<Text>().color = Color.black;
+        }
+        else {
+            button.enabled = false;
+            button.GetComponentInChildren<Text>().color = Color.gray;
+        }
+	}
+}
