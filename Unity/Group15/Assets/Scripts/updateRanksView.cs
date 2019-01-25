7f6900c37f35b4f12fe158b434bfb8ef404a86d9
Unknown <psyam19@nottingham.ac.uk>
2019-01-25 21:55:20 +0000

    Added RandomPvP screen
    Added dummy top5 rankings system - list works, but need to GET top5 players of each rank from our server on demand

---
+++ Unity/Group15/Assets/Scripts/updateRanksView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateRanksView : MonoBehaviour {
+
+    public GameObject display;
+    
+
+    private List<string> getPlayers(string rank) {
+        List<string> players = new List<string>();
+        players.Clear();
+
+        for(int i=0;i<5;i++) {    //here it would GET the top5 from server and add to list.
+            players.Add(rank); 
+        }
+        //players.Sort -- by points.
+
+        return players;
+    }
+
+    public void displayTopPlayers(string rank) {
+        int i = 0;
+        foreach (string current in getPlayers(rank)){
+            string currentslot = "Slot" + (++i);
+            display.transform.Find(currentslot).gameObject.GetComponentInChildren<Text>().text = current;
+        }
+    }
+}
