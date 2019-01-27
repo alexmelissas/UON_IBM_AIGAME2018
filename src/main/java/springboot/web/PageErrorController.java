3c5c1ae31847cba3bf0126ab9d190cd4213aaf32
char <965059585@qq.com>
2019-01-27 17:09:53 +0000

    Handle error  page

--- src/main/java/springboot/web/PageErrorController.java
+++ src/main/java/springboot/web/PageErrorController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageErrorController {
-//	@RequestMapping("/error")
-//	public @ResponseBody String error() {
-//		// TODO
-//		return "Error";
-//	}
+	@RequestMapping("/404")
+	public @ResponseBody String pageNotFound() {
+		return "404 Page Not Found.";
+	}
+	
+	@RequestMapping("/500")
+	public @ResponseBody String exception() {
+		return "Sorrry, authorization fails. Please try again later.";
+	}
 }
