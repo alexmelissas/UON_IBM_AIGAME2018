626fc7590d2446c24dcf8a266b559202b659ba0c
Unknown <psyam19@nottingham.ac.uk>
2019-01-30 18:43:12 +0000

    Login screen / twitter login / overworld
    Added a login/register option for startscreen
    Added placeholder overworld
    Added dummy twitter login functionality
    + more

--- Unity/Group15/Assets/Scripts/registerUser.cs
+++ Unity/Group15/Assets/Scripts/registerUser.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class registerButton : MonoBehaviour
+public class registerUser : MonoBehaviour
 {
 
     public InputField username_field;
