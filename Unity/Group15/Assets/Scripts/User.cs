713654d3b0d949a806b4532c4345d7be278f2b86
Unknown <psyam19@nottingham.ac.uk>
2019-02-12 13:42:57 +0000

    Initial server interaction
    Within the signup screen - basic POST to create user (gets error 500)

---
+++ Unity/Group15/Assets/Scripts/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User{
+
+    public string username { get; set; }
+    public string password { get; set; }
+
+	public User(string name, string pass)
+    {
+        this.username = name;
+        this.password = pass;
+    }
+}
--- Unity/Group15/Assets/Scripts/registerUser.cs
+++ Unity/Group15/Assets/Scripts/registerUser.cs
@@ -1,37 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using System.Net;
+
 public class registerUser : MonoBehaviour
 {
-
     public InputField username_field;
     public InputField password_field;
+
+    IEnumerator TryRegister(string url, string json)
+    {
+        UnityWebRequest uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
 
     public void checkUserPass()
     {
         string username = username_field.text;
         string password = password_field.text;
-        if (username == "" || password == "") 
+        if (username == "" || password == "")
         {
             Debug.Log("EMPTY");
-            return;
         }
-        if(username.Length>25)
+        else if (username.Length > 25)
         {
             Debug.Log("TOO LONG");
-            return;
         }
         else
         {
-            // request with post
+            string url = "http://132.232.30.215:8080/users/";
+            string json = JsonUtility.ToJson(new User(username, password));
+            StartCoroutine(TryRegister(url, json));
         }
-
-        Debug.Log(username + " " + password); //this would pass it to the server and check if available, and save
-        if (true) // this would check if they're ok
-            SceneManager.LoadScene("TwitterLogin");
-        return;
+        return;       
     }
 }
