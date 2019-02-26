ebb437f4e0cdde2944c886320a59b1ed99a8668b
char <965059585@qq.com>
2019-02-26 12:13:40 +0000

    Return Player after submitting the ideal personality

--- src/main/java/springboot/web/IdealController.java
+++ src/main/java/springboot/web/IdealController.java
@@ -9,26 +9,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import springboot.domain.Ideal;
+import springboot.domain.Player;
 import springboot.service.IdealService;
+import springboot.service.PlayerService;
 
 @RestController
 public class IdealController {
 	@Autowired
 	private IdealService idealService;
-
+	@Autowired
+	private PlayerService playerService;
+	
 	@GetMapping("/ideals")
 	public @ResponseBody Iterable<Ideal> getIdeals() {
 		return idealService.getIdeals();
 	}
 	
 	@PutMapping("/ideals/{id}")
-	public @ResponseBody String updateIdeal(@PathVariable String id, @RequestBody Ideal ideal) {
+	public @ResponseBody Player updateIdeal(@PathVariable String id, @RequestBody Ideal ideal) {
 		// UPDATE IDEAL PERSONALITY
 		
 		// TODO exception handle
 		ideal.setId(id);
 		idealService.updateIdeal(id, ideal);
 		System.out.println(ideal);
-		return "Updated";
+		return playerService.getPlayerById(id).get();
 	}
 }
