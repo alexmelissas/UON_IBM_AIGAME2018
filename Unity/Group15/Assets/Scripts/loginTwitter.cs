55c2a20360ad2f0802c8beda6ee2e16744e3cd57
Unknown <psyam19@nottingham.ac.uk>
2019-03-26 13:58:57 +0000

    Simplify stuff

--- Unity/Group15/Assets/Scripts/loginTwitter.cs
+++ Unity/Group15/Assets/Scripts/loginTwitter.cs
@@ -8,6 +8,13 @@ using VoxelBusters.NativePlugins;
 //! Twitter login handler
 public class LoginTwitter : MonoBehaviour {
 
+    private bool otherPopupOpen;
+
+    private void Start()
+    {
+        otherPopupOpen = false;
+    }
+
     //! Keep checking if twitter linkage was OK - until timeout (10s after they return from twitter page)
     private IEnumerator TryTwitter()
     {
@@ -40,34 +47,9 @@ public class LoginTwitter : MonoBehaviour {
                 }
             }
             uwr.Dispose();
-            if (repeats == 5)// NPBinding.UI.ShowToast("Twitter connection failed. Please try again, or play without Twitter.", eToastMessageLength.SHORT);
-            {
-                string[] _buttons = new string[] { "Retry", "Play Without Twitter"};
-                NPBinding.UI.ShowAlertDialogWithMultipleButtons("Personality bot is sad", "Twitter Connection wasn't happy :(", _buttons, OnButtonPressed);
-            }
+            if (repeats == 5) ShowPopup("fail_connection");
         }
         StopCoroutine(TryTwitter());
-    }
-    
-    //! Display terms of usage - for ethics
-    public void ShowTerms()
-    {
-        string[] _buttons = new string[] { "Decline", "Accept" };
-        NPBinding.UI.ShowAlertDialogWithMultipleButtons("Terms of Usage"
-            ,"By agreeing and linking your twitter you agree to " +
-            "allow access to your public Twitter profile, in order " +
-            "to analyse your personality. Only your tweets will be "+
-            "accessed, and you will be made aware when. Your tweets "+
-            "and data is never stored on our servers; the only personal "+
-            "data stored is your factor of similarity between your"+
-            "self-proclaimed ideal personality and your real one, "+
-            "which will be read from your tweets. Your tweets are merely "+
-            "passed temporarily into Watson, which analyses them and gives "+
-            "us a personality analysis which use to calculate your similarity"+
-            "factor, and then deleted."
-            , _buttons, OnButtonPressed);
-
-        return;
     }
 
     //! Try to open Twitter login page
@@ -80,39 +62,68 @@ public class LoginTwitter : MonoBehaviour {
             StartCoroutine(TryTwitter());
         }
     }
-
-    //! Skip twitter login entirely
-    public void SkipTwitter()
+    
+    //! Show popups for twitter terms confirmation/failed connection/skip twitter
+    public void ShowPopup(string popup)
     {
-        string[] _buttons = new string[] { "Link Twitter", "Play without Twitter" };
-        NPBinding.UI.ShowAlertDialogWithMultipleButtons("You're missing out!"
-            , "You can play this game without a Twitter account. \nHowever, by "+
-            "not linking your Twitter profile, you'll be missing out on "+
-            "the entire Personality improvement portion of this game." +
-            "This means lower stats and more effort to reach the top!\n" +
-            "Are you sure you'd like to proceed without Twitter?"
-            , _buttons, OnButtonPressed);
-        
-        return;
+        if(!otherPopupOpen)
+        {
+            otherPopupOpen = true;
+            string[] _buttons;
+            switch (popup)
+            {
+                case "fail_connection":
+                    _buttons = new string[] { "Retry", "Play Without Twitter" };
+                    NPBinding.UI.ShowAlertDialogWithMultipleButtons("Personality bot is sad",
+                        "Twitter Connection wasn't happy :(", _buttons, OnButtonPressed);
+                    break;
+
+                case "show_terms":
+                    _buttons = new string[] { "Decline", "Accept" };
+                    NPBinding.UI.ShowAlertDialogWithMultipleButtons("Terms of Usage"
+                        , "By agreeing and linking your twitter you agree to " +
+                        "allow access to your public Twitter profile, in order " +
+                        "to analyse your personality. Only your tweets will be " +
+                        "accessed, and you will be made aware when. Your tweets " +
+                        "and data is never stored on our servers; the only personal " +
+                        "data stored is your factor of similarity between your" +
+                        "self-proclaimed ideal personality and your real one, " +
+                        "which will be read from your tweets. Your tweets are merely " +
+                        "passed temporarily into Watson, which analyses them and gives " +
+                        "us a personality analysis which use to calculate your similarity" +
+                        "factor, and then deleted."
+                        , _buttons, OnButtonPressed);
+                    break;
+
+                case "skip_twitter":
+                    _buttons = new string[] { "Link Twitter", "Play without Twitter" };
+                    NPBinding.UI.ShowAlertDialogWithMultipleButtons("You're missing out!"
+                        , "You can play this game without a Twitter account. \nHowever, by " +
+                        "not linking your Twitter profile, you'll be missing out on " +
+                        "the entire Personality improvement portion of this game." +
+                        "This means lower stats and more effort to reach the top!\n" +
+                        "Are you sure you'd like to proceed without Twitter?"
+                        , _buttons, OnButtonPressed);
+                    break;
+            }
+        }
     }
 
-    private void PlayWithoutTwitter()
-    {
-        gameObject.AddComponent<ChangeScene>().Forward("CharacterCreation");
-    }
 
     //! Handle dialog box button options
     private void OnButtonPressed(string _buttonPressed)
     {
         switch(_buttonPressed)
         {
-            case "Retry": ShowTerms(); break;
-            case "Play Without Twitter": SkipTwitter(); break;
+            case "Retry": ShowPopup("show_terms");  break;
+            case "Play Without Twitter": ShowPopup("skip_twitter"); break;
             case "Accept": TryOpenTwitter(true); break;
             case "Decline": TryOpenTwitter(false); break;
-            case "Link Twitter": ShowTerms(); break;
-            case "Play without Twitter": PlayWithoutTwitter(); break;
+            case "Link Twitter": otherPopupOpen = false; ShowPopup("show_terms"); break;
+            case "Play without Twitter":gameObject.AddComponent<ChangeScene>().Forward("CharacterCreation");
+                break;
         }
+        if(_buttonPressed!="Link Twitter")otherPopupOpen = false;
         return;
     }
 }
